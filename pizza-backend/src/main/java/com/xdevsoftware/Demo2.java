673e613a67a60c46bd42805c5940ce45d58c3bfe
Richard Fichtner <r.fichtner@xdev-software.de>
2021-12-07 18:17:27 +0100

    add the maven-profiler extension

---
+++ pizza-backend/src/main/java/com/xdevsoftware/Demo2.java
@@ -0,0 +1,17 @@
+/* package com.xdevsoftware;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+
+public class Demo
+{
+	
+	public static void main(final String[] args) throws Exception
+	{
+		new Thread(() -> System.out.println("Hello lambda")).start();
+		System.out.println(BeanUtils.describe("Hello bean"));
+	}
+}
+
+*/
+
