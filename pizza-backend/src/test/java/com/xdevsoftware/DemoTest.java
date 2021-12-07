6a089bfa8ad81d238fd8570dfe0a6fd6fcb2e676
Richard Fichtner <r.fichtner@xdev-software.de>
2021-12-07 17:08:42 +0100

    lets add a unit test and junit dependency

---
+++ pizza-backend/src/test/java/com/xdevsoftware/DemoTest.java
@@ -0,0 +1,17 @@
+package com.xdevsoftware;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class DemoTest
+{
+	
+	@Test	
+	void someTest()
+	{
+		assertTrue(true);
+	}
+	
+}
