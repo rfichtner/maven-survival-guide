c9c429d3f69ca28f5610a82720eb8e53e892d1f4
Richard Fichtner <r.fichtner@xdev-software.de>
2022-08-29 10:20:50 +0200

    added demosteps

---
+++ demosteps.ps1
@@ -0,0 +1,34 @@
+pause
+git checkout f0c18237df9a895b017ad9dda225b041497e2dbb
+git log -1 --pretty=format:"%s"
+pause
+git checkout 6a089bfa8ad81d238fd8570dfe0a6fd6fcb2e676
+git log -1 --pretty=format:"%s"
+pause
+git checkout 897fe8b422bb4fb195a2a8060ba2c36b10905aed
+git log -1 --pretty=format:"%s"
+pause
+git checkout 1c9c2b75053b811db8de6eadba07c2c6ea29e54d
+git log -1 --pretty=format:"%s"
+pause
+git checkout cadd5ddc9e15d76f03f546628f03148e794a6e4c
+git log -1 --pretty=format:"%s"
+pause
+git checkout a0ab44b5a4237530c823d21d8a544226748ffc80
+git log -1 --pretty=format:"%s"
+pause
+git checkout 6536c512cf95c5dddfa33b4f20f7a4793d861519
+git log -1 --pretty=format:"%s"
+pause
+git checkout 0813b334f19564fa87586e757c4c7b0248bdd7fc
+git log -1 --pretty=format:"%s"
+pause
+git checkout 377ee7f90884d6acecebbb6fa501f39f5b2ecad8
+git log -1 --pretty=format:"%s"
+pause
+git checkout bcdabf8bec464b5fa0cdcbe8b3f5b6efbc1a784b
+git log -1 --pretty=format:"%s"
+pause
+git checkout Louisiana
+git log -1
+pause
