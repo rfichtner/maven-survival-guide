bcbbdaea2dbd7b75c91b40edfdbcceb05ab0861e
Richard Fichtner <r.fichtner@xdev-software.de>
2022-08-29 09:31:21 +0200

    add some dependencies

--- pizza-backend/src/main/java/com/xdevsoftware/Demo.java
+++ pizza-backend/src/main/java/com/xdevsoftware/Demo.java
@@ -1,12 +1,38 @@
 package com.xdevsoftware;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import one.microstream.storage.embedded.types.EmbeddedStorage;
+import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+
 public class Demo
 {
 	
-	public static void main(final String[] args)
+
+
+	public static void main(final String[] args) throws Exception
 	{
-		System.out.println("Hello world");
-		new Thread(()-> System.out.println("Hello lambda")).start();
+		new Thread(() -> System.out.println("Hello lambda")).start();
+		System.out.println(BeanUtils.describe("Hello bean"));
+		
+		List<LocalDateTime> runs = new ArrayList<>();
+
+		// Initialize a storage manager ("the database") with purely defaults.
+		final EmbeddedStorageManager storage = EmbeddedStorage.start(runs);
+		
+		runs.add(LocalDateTime.now());
+		runs.stream().forEach(System.out::println);
+		
+		// store
+		storage.storeRoot();
+
+		// shutdown storage
+		storage.shutdown();
+		
 	}
 	
 }
