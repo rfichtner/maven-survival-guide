ca7a4234e327ec081909378fc0285b22964d1e7f
Richard Fichtner <r.fichtner@xdev-software.de>
2024-06-17 15:45:11 +0200

    Check for forbidden APIs

---
+++ pizza-backend/src/main/java/Write.java
@@ -0,0 +1,14 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Write {
+    public void writeToFile() throws IOException {
+        String str = "Hello";
+        FileOutputStream outputStream = new FileOutputStream("file.data");
+        byte[] strToBytes = str.getBytes();
+        outputStream.write(strToBytes);
+
+        outputStream.close();
+        System.out.println("done writing");
+    }
+}
