c8cc6002431999e56740cd974565cb105198d058
Richard Fichtner <r.fichtner@xdev-software.de>
2025-03-16 10:56:30 -0700

    Update demosteps.ps1

--- demosteps.ps1
+++ demosteps.ps1
@@ -2,34 +2,52 @@ pause
 git checkout f0c18237df9a895b017ad9dda225b041497e2dbb --force
 git log -1 --pretty=format:"%s"
 pause
+cls
 git checkout 6a089bfa8ad81d238fd8570dfe0a6fd6fcb2e676 --force
 git log -1 --pretty=format:"%s"
 pause
+cls
 git checkout 897fe8b422bb4fb195a2a8060ba2c36b10905aed --force
 git log -1 --pretty=format:"%s"
 pause
+cls
 git checkout 1c9c2b75053b811db8de6eadba07c2c6ea29e54d --force
 git log -1 --pretty=format:"%s"
 pause
+cls
 git checkout cadd5ddc9e15d76f03f546628f03148e794a6e4c --force
 git log -1 --pretty=format:"%s"
 pause
-git checkout a0ab44b5a4237530c823d21d8a544226748ffc80 --force
+cls
+git checkout 7f8dcabee030df851f7791bebdcdb1789a30d6e4 --force
 git log -1 --pretty=format:"%s"
 pause
-git checkout 6536c512cf95c5dddfa33b4f20f7a4793d861519 --force
+cls
+git checkout bcbbdaea2dbd7b75c91b40edfdbcceb05ab0861e --force
 git log -1 --pretty=format:"%s"
 pause
-git checkout 0813b334f19564fa87586e757c4c7b0248bdd7fc --force
+cls
+git checkout 4b495b9f0d422fccd878e21d1025d927296e7ce7 --force
 git log -1 --pretty=format:"%s"
 pause
-git checkout 377ee7f90884d6acecebbb6fa501f39f5b2ecad8 --force
+cls
+git checkout c5cf255e91ae3f989efa67b471268f55de5b7b79 --force
 git log -1 --pretty=format:"%s"
 pause
+cls
+git checkout fc7986987fe2cd6a5e0265b09b6fc98fcde577b6 --force
+git log -1 --pretty=format:"%s"
+pause
+cls
+git checkout 802892b3d5ec9d00fd754be9c8bac26e13a692b4 --force
+git log -1 --pretty=format:"%s"
+pause
+cls
 Invoke-Item .\pizza-backend\target\site\dependencies.html
 start-sleep 1
 Invoke-Item .\pizza-backend\target\site\third-party-report.html
 pause
+cls
 git checkout JavaOne25 --force
 git log -1
 pause
